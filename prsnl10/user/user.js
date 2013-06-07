2175f8c9a01011aea77ac4ced0ee09bbf8b28081
Alessandro Lorenzi <alorenzi@alorenzi01.extraordy.com>
2013-06-07 19:36:10 +0200

    first

---
+++ prsnl10/user/user.js
@@ -0,0 +1,32 @@
+/* en: Place for user defined JavaScript - this file can safely be preserved
+   when updating. See README for details.
+   ATTENTION: Do not forget to activate the template option
+              "prsnl10_loaduserjs" (->"Load 'prsnl10/user/user.js'?") in the
+              DokuWiki Config Manager! Otherwise, any changes to this file
+              won't have any effect!
+   
+   de: Ort für benutzerdefiniertes JavaScript - Diese Datei kann beim
+   Durchführen von Updates ohne Risiko beibehalten werden. Konsultieren Sie
+   die README für Detailinformationen.
+
+   ACHTUNG: Vergessen Sie nicht die Template-Option "prsnl10_loaduserjs"
+            (->"Datei 'prsnl10/user/user.js' laden?") im DokuWiki Config
+            Manager zu aktivieren! Andernfalls werden sämtliche Änderungen an
+            dieser Datei ohne Auswirkungen bleiben! */
+
+
+
+function change_cover(){
+	p = document.getElementsByTagName("p");
+
+	for(index =0; index < p.length ; index++){
+		if(img= p[index].getElementsByTagName("img")[0])
+		{
+			document.getElementById("cover").src=img.src+"&w=";
+			return;
+		}
+			
+	}
+	
+}
+change_cover();
